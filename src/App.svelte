32df0e12b7c618060257a7c22b52b95dc2eb9a90
ForestPotato <anthony.jameswhite2001@gmail.com>
2022-01-21 09:50:56 +0000

    Svelte Grid & Clock component

---
+++ src/App.svelte
@@ -0,0 +1,89 @@
+<script>
+	import  Clock  from './components/clock.svelte'
+</script>
+
+<div id="app">
+	<div class="grid-container">
+		<div class="grid-item grid-item1">
+			<Clock />
+		</div>
+		<div class="grid-item grid-item2">f</div>
+		<div class="grid-item grid-item3">f</div>
+		<div class="grid-item grid-item4">
+			f<!--<Table :peoples="people" @delete-task="deleteTask"/>-->
+		</div>
+		<div class="grid-item grid-item5">
+			f<!--<TableSubmit @submit="addNewRow" />-->
+		</div>
+		<div class="grid-item grid-item6">f</div>
+		<div class="grid-item grid-item7">f</div>
+		<div class="grid-item grid-item8">f</div>
+		<div class="grid-item grid-item9">f</div>
+	</div>
+</div>
+
+<style>
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		color: white;
+		width: 98%;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	.grid-container {
+		position: relative;
+		padding-top: 1.6rem;
+		width: 95%;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	.grid-item {
+		position: relative;
+		font-size: 1.6rem;
+		padding-top: 3.1rem;
+		background-color: #379ad6;
+		color: #222;
+		border: 0.1rem solid white;
+		overflow: auto;
+	}
+	/* Main CSS */
+	.grid-container {
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: repeat(9, 20rem);
+		grid-row-gap: 1.25rem;
+	}
+	/* Media queries */
+	/* Small devices (portrait tablets and large phones, 600px and up) */
+	@media only screen and (min-width: 600px) {
+		.grid-container {
+			grid-template-columns: repeat(2, 1fr);
+			grid-column-gap: 0.5rem;
+		}
+		/*.input-control {
+			width: 80%;
+		}*/
+	}
+	/* Large devices (laptops/desktops, 992px and up) */
+	@media only screen and (min-width: 992px) {
+		.grid-container {
+			grid-template-rows: repeat(9, 22rem);
+		}
+		/*.p-fontSize {
+			font-size: medium;
+		}*/
+	}
+	/* Extra large devices (large laptops and desktops, 1200px and up) */
+	@media only screen and (min-width: 1279px) {
+		.grid-container {
+			grid-template-rows: repeat(9, 25rem);
+			grid-template-columns: repeat(3, 1fr);
+		}
+		/*.img-text {
+			margin-top: -0.1rem;
+		}
+		.circle p {
+			font-size: 16px;
+		}*/
+	}
+</style>
---
+++ src/components/clock.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { onMount, onDestroy } from 'svelte'
+
+	let clock = new Date();
+	$:hours = clock.getHours();
+	$:minutes = clock.getMinutes();
+	$:seconds = clock.getSeconds();
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			clock = new Date();
+		}, 1000);
+	});
+
+	onDestroy(() => {
+		clearInterval(interval);
+	});
+
+</script>
+
+<h2 class="clock-container">
+    <span class="clock-time">
+		{hours <= 9 ? "0" + hours : hours}:
+        {minutes <= 9 ? "0" + minutes : minutes}:
+        {seconds <= 9 ? "0" + seconds : seconds}
+	</span>
+</h2>
+
+<style>
+	.clock-container {
+		width: 100%;
+		height: auto;
+		margin: 0;
+  		position: absolute;
+  		top: 50%;
+  		-ms-transform: translateY(-50%);
+  		transform: translateY(-50%);
+		text-align: center;
+		color: #fff;
+	}
+</style>
